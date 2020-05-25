25527b78162a6193866744ce8828acc2a68d5907
Johan Hidding <j.hidding@esciencecenter.nl>
2020-05-25 14:08:13 +0200

    remove old tangle tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,13 +2,10 @@ module Main where
 
 import Test.Hspec
 
-import Config (readLocalConfig)
 import ListStreamSpec (listStreamSpec)
-import TangleSpec (tangleSpec, tangleEqualSpec)
 import TextUtilSpec (textUtilSpec)
 
 main :: IO ()
-main = do
-    cfg <- readLocalConfig
-    hspec $ listStreamSpec >> tangleSpec cfg >> tangleEqualSpec cfg >> textUtilSpec
+main =
+    hspec $ listStreamSpec >> textUtilSpec
 
