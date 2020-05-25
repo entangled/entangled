c4788902ea425d484e05ff66ff7c6441aeca733f
Johan Hidding <j.hidding@esciencecenter.nl>
2020-05-25 12:32:27 +0200

    add formatMaybe function

--- src/Format.hs
+++ src/Format.hs
@@ -3,11 +3,11 @@ module Format where
 
 import RIO
 import qualified RIO.Map as M
-import Text.Megaparsec (Parsec, chunk, takeWhile1P)
+import Text.Megaparsec (Parsec, chunk, takeWhile1P, parseMaybe)
 import Errors (EntangledError(..))
 
 type Parser = Parsec Void Text
-data SpecItem = Plain Text | Variable Text
+data SpecItem = Plain Text | Variable Text deriving (Show)
 type Spec = [SpecItem]
 
 specVariableP :: Parser SpecItem
@@ -33,9 +33,17 @@ specPlainTextP = Plain <$> takeWhile1P Nothing (`notElem` ['{', '}'])
 specP :: Parser Spec
 specP = many (specPlainTextP <|> specBraceLeftP <|> specBraceRightP <|> specVariableP)
 
+spec :: Text -> Maybe Spec
+spec = parseMaybe specP
+
 formatM :: (MonadThrow m) => Spec -> Map Text Text -> m Text
-formatM spec args = mconcat <$> mapM resolve spec
+formatM s args = mconcat <$> mapM resolve s
     where resolve (Plain t) = return t
           resolve (Variable v) = maybe (throwM $ ConfigError $ "unknown variable '" <> v <> "'")
                                        return (args M.!? v)
+
+formatMaybe :: Spec -> Map Text Text -> Maybe Text
+formatMaybe s args = mconcat <$> mapM resolve s
+    where resolve (Plain t) = Just t
+          resolve (Variable v) = args M.!? v
 
