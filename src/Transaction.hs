7a801fd7fc48dfd0797cd4f9f40ef14bce9e4b25
Johan Hidding <j.hidding@esciencecenter.nl>
2022-01-24 16:31:52 +0100

    update example-config

--- src/Transaction.hs
+++ src/Transaction.hs
@@ -6,17 +6,23 @@ module Transaction where
 import RIO
 import qualified RIO.Text as T
 -- ~\~ begin <<lit/a4-fileio.md|transaction-imports>>[0]
--- import qualified Data.Text.Prettyprint.Doc as P
 import Console (Doc, group, msgCreate, msgDelete, msgWrite)
 import qualified Console
 import qualified Data.Text.IO as T.IO
 -- ~\~ end
 
+-- ~\~ begin <<lit/a4-fileio.md|transaction>>[0]
 data Description = Message Doc
                  | CreateFile FilePath
                  | WriteFile FilePath
                  | DeleteFile FilePath
 
+data Transaction m = Transaction
+  { action :: Maybe (m ())
+  , description :: [Description]
+  , needConfirm :: Bool }
+-- ~\~ end
+-- ~\~ begin <<lit/a4-fileio.md|transaction>>[1]
 showDescriptionHuman :: [Description] -> Doc
 showDescriptionHuman = mconcat . map (\case
     Message x    -> x
@@ -30,13 +36,8 @@ showDescriptionMachine = T.unlines . mapMaybe (\case
     CreateFile f -> Just $ "+ " <> T.pack f
     WriteFile f  -> Just $ "~ " <> T.pack f
     DeleteFile f -> Just $ "- " <> T.pack f)
-
-data Transaction m = Transaction
-  { action :: Maybe (m ())
-  , description :: [Description]
-  , needConfirm :: Bool }
-
--- ~\~ begin <<lit/a4-fileio.md|transaction>>[0]
+-- ~\~ end
+-- ~\~ begin <<lit/a4-fileio.md|transaction>>[2]
 instance (Semigroup (m ())) => Semigroup (Transaction m) where
     (Transaction al dl cl) <> (Transaction ar dr cr)
       = Transaction (al <> ar) (dl <> dr) (cl || cr)
@@ -44,7 +45,7 @@ instance (Semigroup (m ())) => Semigroup (Transaction m) where
 instance (Monoid (m ())) => Monoid (Transaction m) where
     mempty = Transaction mempty mempty False
 -- ~\~ end
--- ~\~ begin <<lit/a4-fileio.md|transaction>>[1]
+-- ~\~ begin <<lit/a4-fileio.md|transaction>>[3]
 plan :: (Monad m) => Description -> m () -> Transaction m
 plan d action = Transaction (Just action) [d] False
 
@@ -54,7 +55,7 @@ doc x = Transaction Nothing [Message x] False
 confirm :: Transaction m
 confirm = Transaction Nothing mempty True
 -- ~\~ end
--- ~\~ begin <<lit/a4-fileio.md|transaction>>[2]
+-- ~\~ begin <<lit/a4-fileio.md|transaction>>[4]
 testTransaction :: (MonadIO m) => Transaction m -> m Bool
 testTransaction (Transaction Nothing _ _)  = return False
 testTransaction (Transaction (Just _) d _) = liftIO $ T.IO.putStr (showDescriptionMachine d) >> return True
